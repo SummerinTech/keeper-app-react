4d1ff77653d2718bc6973e8f1ec31fc40cbd7fe0
Summer <72228259+SummerinTech@users.noreply.github.com>
2022-02-01 13:19:52 -0800

    Set up starter files and design for Keep app

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="Footer">
+			<footer>
+				<a href="/">Open-Source Code</a> Created by Summer Robinson | 2022
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
